33f4a9d1658c1afed1bc36dc42f2ecf699c989c9
ASEO-FR <aseobobe@gmail.com>
2025-12-14 21:37:00 +0100

    refactor: Suppression de l'ancien contrôleur audio flottant (ménage)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import GlobalAudioController from "@/components/GlobalAudioController/GlobalAudioController";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,7 +28,6 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={inter.variable}>
-        <GlobalAudioController />
         {children}
       </body>
     </html>
